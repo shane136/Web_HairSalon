ee02e942b94db3c654c6a65bc4bbe2e4305eb80a
shane136 <preashane5@gmail.com>
2020-12-23 11:50:24 +0800

    updated_header

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ session_start();
 <html>
 
 <head>
-	<title>JEREHARIS Hair Salon</title>
+	<title>J.HairSalon|Login</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
