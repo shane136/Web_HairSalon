27323dfa180f7d517ace3eebea988b92e448d6db
cris-io <crisargie.ecleo@g.msuiit.edu.ph>
2021-01-09 16:41:16 +0800

    ah

--- admin/manageService.php
+++ admin/manageService.php
@@ -17,6 +17,9 @@ $user_id = $_SESSION['user_id'];
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body class = "d-flex flex-row h-100">
@@ -42,18 +45,21 @@ $user_id = $_SESSION['user_id'];
 
 
 <div class="container mh-100 p-3" style="background: #0F222D;height:20vh;">
+
 <div class="h-100 rounded d-flex justify-content-center" style="background:  #ffe6e6;">
 <img src="\Web_HairSalon\image\logo.png" alt="" class="h-100" style="border-radius: 50%;">
 </div>
 
-<div class="col-sm-8 p-1 mt-4" style ="background: #ffe6e6; border-radius: 10px;">
-<div class="card shadow mb-0">
-<div class="card-header py-4">
+<div class="container-fluid col-sm-12 p-1 mt-4" style ="background: #ffe6e6; border-radius: 10px;">
+<div class="row p-5" >
+<div class="col-lg-6 p-1">
+    <div class="card shadow mb-0">
+    <div class="card-header py-4">
                 <h9 class="m-0 font-weight-bold text-primary">Services</h9>
-</div>
+    </div>
             <div class="card-body">
                 <div class="table-responsive-sm">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <table class="table table-bordered p-3" id="dataTable" width="100%">
                         <thead>
                             <tr>
                                 <th>ID</th>
@@ -63,35 +69,83 @@ $user_id = $_SESSION['user_id'];
                             </tr>
                         </thead>
                         <tbody>
-                        	<?php
+                            <?php
 $product_query = "SELECT * from services, service_type WHERE services.type_id = service_type.type_id;";
 $all_products = mysqli_query($con, $product_query);
 $resultCount = mysqli_num_rows($all_products);
 
-                        		if($resultCount>0){
-
-                        			while ($products = mysqli_fetch_assoc($all_products)) {
-                        				echo '<tr>';
-                        				echo '<td>'.$products['service_id'].'</td>';
-	                            	echo '<td>'.$products['service_name'].'</td>';
-	                            	echo '<td>'.$products['service_price'].'</td>';
-	                            	echo '<td>'.$products['type_name'].'</td>';
-	                            	echo '</tr>';
-                        			}
-                        		}
-                        	?>
+                                if($resultCount>0){
+                                $i=0;
+                                    while ($products = mysqli_fetch_assoc($all_products)) {
+                                        echo '<tr>';
+                                        $i=$i+1;
+                                    ?>
+                                        <td><?php echo $i;?></td>
+                                    <?php
+                                    echo '<td>'.$products['service_name'].'</td>';
+                                    echo '<td>'.$products['service_price'].'</td>';
+                                    echo '<td>'.$products['type_name'].'</td>';
+                                    echo '</tr>';
+                                    }
+                                }
+                            ?>
                         </tbody>
                     </table>
                 </div>
             </div>
-  	</div>
+    </div>
+</div>
+<div class="col-lg-6 p-1">
+    <div class="col-sm-12 d-flex" style="border: 1px solid #fff; border-radius: 10px; background-color: #fff; height: 100%;">
+        <div class="p-4">
+<h2>Menu Services</h2>
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Color</a></li>
+    <li><a data-toggle="tab" href="#menu1">Styling</a></li>
+    <li><a data-toggle="tab" href="#menu2">Waxing</a></li>
+    <li><a data-toggle="tab" href="#menu3">Extensions</a></li>
+    <li><a data-toggle="tab" href="#menu4">Design</a></li>
+    <li><a data-toggle="tab" href="#menu5">Grooming</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+      <h3>Color</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>Menu 1</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Menu 3</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu4" class="tab-pane fade">
+      <h3>Menu 4</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+    <div id="menu5" class="tab-pane fade">
+      <h3>Menu 5</h3>
+      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+  </div>
+        </div>
+    </div>    
+</div>
 </div>
 </div>
 
 </div>
 
+</div>
 <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <script src="../js/demo/datatables-demo.js"></script>
+
 </body>
 </html>
