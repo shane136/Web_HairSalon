b9437f1cd991e2bf914a80c52dff4564afe26b27
cris-io <crisargie.ecleo@g.msuiit.edu.ph>
2021-01-10 23:42:22 +0800

    Update empProfile.php

--- employee/empProfile.php
+++ employee/empProfile.php
@@ -1,4 +1,28 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT']."/Web_HairSalon/conn/connection.php");
+$user_id = $_SESSION['user_id'];
+$sql = mysqli_query($con, "SELECT * from employee, user_account WHERE employee.user_id = user_account.user_id;");
+$conResult = mysqli_fetch_assoc($sql);
 
+	if($conResult['job_type'] == 1){
+	  $type = "Colourist";
+	}
+	elseif($conResult['job_type'] == 2){
+	  $type = "Waxing Specialist";
+	}
+	elseif($conResult['job_type'] == 3){
+	  $type = "Barber Stylist";
+	}
+	elseif($conResult['job_type'] == 4){
+	  $type = "Hair Extenstion Stylist";
+	}
+	elseif($conResult['job_type'] == 5){
+	  $type = "Senior Stylist";
+	}
+	else{
+	  $type = "Hairdresser";
+	}
+?>
 <!DOCTYPE html>
 <html class = "h-100"lang="en" dir="ltr">
 <head>
@@ -33,33 +57,38 @@
 </div>
 
 <div class="container-fluid col-lg-12 p-1 mt-4" style ="background: #ffe6e6; border-radius: 10px;">
-<div class="col-xl-8 order-xl-1">
+	
+<div class="col-lg-12 p-3">
           <div class="card bg-secondary shadow">
-            <div class="card-header bg-white border-0">
+            <div class="card-header border-0">
               <div class="row align-items-center">
                 <div class="col-8">
                   <h3 class="mb-0">My account</h3>
                 </div>
-                <div class="col-4 text-right">
-                  <a href="#!" class="btn btn-sm btn-primary">Settings</a>
-                </div>
               </div>
             </div>
-            <div class="card-body">
+            <div class="card-body bg-white">
               <form>
-                <h6 class="heading-small text-muted mb-4">User information</h6>
+                <div class="row col-12">
+                <div class="col-6">
+                  <h6 class="heading-small text-muted mb-4">User information</h6>
+                </div>
+                <div class="col-1">                  
+                  <button type="button" data-toggle="modal" data-target="#edit" class="btn btn-primary" style="float: right;">Settings</button>
+                </div>
+                </div>
                 <div class="pl-lg-4">
                   <div class="row">
                     <div class="col-lg-6">
                       <div class="form-group focused">
                         <label class="form-control-label" for="input-username">Username</label>
-                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="lucky.jesse">
+                        <input type="text" id="input-username" class="form-control form-control-alternative" placeholder="Username" value="<?php echo $conResult['user_name']; ?>">
                       </div>
                     </div>
                     <div class="col-lg-6">
                       <div class="form-group">
                         <label class="form-control-label" for="input-email">Email address</label>
-                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="jesse@example.com">
+                        <input type="email" id="input-email" class="form-control form-control-alternative" placeholder="<?php echo $conResult['email']; ?>">
                       </div>
                     </div>
                   </div>
@@ -67,13 +96,13 @@
                     <div class="col-lg-6">
                       <div class="form-group focused">
                         <label class="form-control-label" for="input-first-name">First name</label>
-                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="Lucky">
+                        <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="<?php echo $conResult['f_name']; ?>">
                       </div>
                     </div>
                     <div class="col-lg-6">
                       <div class="form-group focused">
                         <label class="form-control-label" for="input-last-name">Last name</label>
-                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="Jesse">
+                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="<?php echo $conResult['l_name']; ?>">
                       </div>
                     </div>
                   </div>
@@ -83,48 +112,113 @@
                 <h6 class="heading-small text-muted mb-4">Contact information</h6>
                 <div class="pl-lg-4">
                   <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-6">
                       <div class="form-group focused">
                         <label class="form-control-label" for="input-address">Address</label>
-                        <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                        <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="<?php echo $conResult['address']; ?>" type="text">
                       </div>
-                    </div>
                   </div>
-                  <div class="row">
-                    <div class="col-lg-4">
+                  <div class="col-md-6">
                       <div class="form-group focused">
-                        <label class="form-control-label" for="input-city">City</label>
-                        <input type="text" id="input-city" class="form-control form-control-alternative" placeholder="City" value="New York">
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="form-group focused">
-                        <label class="form-control-label" for="input-country">Country</label>
-                        <input type="text" id="input-country" class="form-control form-control-alternative" placeholder="Country" value="United States">
-                      </div>
-                    </div>
-                    <div class="col-lg-4">
-                      <div class="form-group">
-                        <label class="form-control-label" for="input-country">Postal code</label>
-                        <input type="number" id="input-postal-code" class="form-control form-control-alternative" placeholder="Postal code">
+                        <label class="form-control-label" for="input-contact-name">Phone number</label>
+                        <input type="text" id="input-contact-name" class="form-control form-control-alternative" placeholder="Last name" value="<?php echo $conResult['phone_number']; ?>">
                       </div>
                     </div>
                   </div>
                 </div>
                 <hr class="my-4">
-                <!-- Description -->
-                <h6 class="heading-small text-muted mb-4">About me</h6>
+                <h6 class="heading-small text-muted mb-4">Job information</h6>
                 <div class="pl-lg-4">
-                  <div class="form-group focused">
-                    <label>About Me</label>
-                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="form-group focused">
+                        <label class="form-control-label" for="input-job">Job</label>
+                        <input id="input-job" class="form-control form-control-alternative" placeholder="Home Address" value="<?php echo $type; ?>" type="text">
+                      </div>
+                    </div>
                   </div>
                 </div>
+                
               </form>
             </div>
           </div>
         </div>
 </div>
+<div class="modal fade" id="edit" role="dialog">
+<div class="modal-dialog">
+
+  <!-- Modal content-->
+  <div class="modal-content">
+    <div class="modal-header" style="padding:20px 50px;">
+      <button type="button" class="close" data-dismiss="modal" title="Close">&times;</button>
+      <h3 style="padding: 10px 30% 0px 0px"><b>Settings</b></h3>
+    </div>
+
+        <div class="modal-body" style="padding:40px 50px;">
+
+      <form class="form-horizontal" action="#" name="form" method="post">
+
+        <div class="form-group">
+          <label class="col-sm-4 control-label">Full Name</label>
+          <div class="row col-12">
+          	<div class="col-6">
+            	<input type="text" name="fname" class="form-control" placeholder="<?php echo $conResult['f_name']; ?>" required="required">
+            </div>
+            <div class="col-6">
+            	<input type="text" name="lname" class="form-control" placeholder="<?php echo $conResult['l_name']; ?>" required="required">
+            </div>
+          </div>
+        </div>
+        <hr class="my-4">
+        <div class="form-group">
+        	<label class="col-sm-4 control-label">Email</label>
+        	<div class="row col-12">
+        		<div class="col-12">
+        			<input type="text" name="email" class="form-control" placeholder="<?php echo $conResult['email']; ?>" required="required">
+        		</div>
+        	</div>
+        	<label class="col-sm-4 control-label">Address</label>
+        	<div class="row col-12">
+        		<div class="col-12">
+        			<input type="text" name="address" class="form-control" placeholder="<?php echo $conResult['address']; ?>" required="required">
+        		</div>
+        	</div>
+        	<label class="col-sm-4 control-label">Phone#</label>
+        	<div class="row col-12">
+        		<div class="col-12">
+        			<input type="text" name="pnumber" class="form-control" placeholder="<?php echo $conResult['phone_number']; ?>" required="required">
+        		</div>
+        	</div>
+        </div>
+<hr class="my-4">
+        <div class="form-group">
+          <label class="col-sm-4 control-label">Username</label>
+          <div class="col-sm-8">
+            <input type="text" name="username" class="form-control" placeholder="<?php echo $conResult['user_name']; ?>" required="required">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label class="col-sm-4 control-label">Password</label>
+          <div class="col-sm-8">
+            <input type="text" name="password" class="form-control" placeholder="<?php echo $conResult['user_password']; ?>" required="required">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label class="col-sm-4 control-label"></label>
+          <div class="col-sm-8">
+            <input type="submit" name="submit" class="btn btn-success" value="Confirm">
+            <input type="reset" name="" class="btn btn-danger" value="Clear Fields">
+          </div>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+</div>
+
 </div>
 
 </body>
