e5ce27a418bf228b61e61b96db99c5694285930b
cris-io <crisargie.ecleo@g.msuiit.edu.ph>
2021-01-14 16:52:22 +0800

    ah :aerial_tramway:

--- employee/empBookings.php
+++ employee/empBookings.php
@@ -18,11 +18,19 @@ $emid = $empl['employee_id'];
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- Bootstrap Core CSS -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="../css/startmin.css" rel="stylesheet">
+<link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 <style type="text/css">
 *{
 	font-family: var(--bs-font-sans-serif);
+}
+span{
+  float: left;
 }
 </style>
 <body class = "d-flex flex-row h-100">
@@ -58,12 +66,48 @@ $sql = mysqli_query($con, "SELECT * from bookings WHERE employee_id = $emid ORDE
   <p id="date" style="float: left; font-size: 25px;"></p>
 </div>
 <div class="card-body">
-    <div class="col-12 p-3" style="border: 1px solid #000; height: 400px;">      
-        <div id="listingTable"></div>
-      <br>
-      <section>page: <span id="page"></span></section>
-      <a href="javascript:prevPage()" id="btn_prev">Prev</a>
-      <a href="javascript:nextPage()" id="btn_next">Next</a>
+    <div class="col-12 p-3" style="border: 1px solid #000; height: 100%;">      
+  <div class="chat-panel panel panel-default" >
+    <!-- /.panel-heading -->
+    <div class="card-body">
+        <ul class="chat">
+<?php
+$sql = mysqli_query($con, "SELECT * from bookings WHERE employee_id = $emid ORDER BY date_sched ASC;");
+          while ($wow = mysqli_fetch_assoc($sql)) {
+                  $date = date_create($wow['date_sched']);
+                  $boDe = date_create($wow['book_date']);
+                  $customer=$wow['customer_id'];
+                  $service=$wow['service_id'];
+                  $sqli=mysqli_query($con,"SELECT * FROM customer WHERE customer_id=$customer;");
+                  $reCus = mysqli_fetch_assoc($sqli);
+                  $sqli=mysqli_query($con,"SELECT * FROM services WHERE service_id=$service;");
+                  $reSer = mysqli_fetch_assoc($sqli);
+  ?>          
+            <li class="right">
+<!--                    <span class="chat-img pull-left">
+                        <img src="http://placehold.it/50/55C1E7/fff" alt="User Avatar"
+                             class="img-circle"/>
+                    </span>
+-->
+                <div class="chat-body clearfix">
+                    <div class="header">
+                        <strong><?php echo $reCus['f_name'].' '.$reCus['l_name'];?></strong>
+                        <small class="pull-right text-muted">
+                            <i class="fa fa-clock-o fa-fw"></i><?php echo date_format($date, "F - d, Y: g:i A");?>
+                        </small>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum
+                        ornare dolor, quis ullamcorper ligula sodales.
+                    </p>
+                </div>
+            </li>
+<?php }
+?>
+        </ul>
+    </div>
+    <!-- /.panel-body -->
+<!-- /.chat panel enddahdahdah -->
     </div>        
 </div>
 <hr>
@@ -76,88 +120,12 @@ $sql = mysqli_query($con, "SELECT * from bookings WHERE employee_id = $emid ORDE
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<script>
-var current_page = 1;
-var records_per_page = 3;
-
-var objJson = [
-<?php
-$menu = array();
-$num = mysqli_num_rows($sql);
-  while($rows=mysqli_fetch_assoc($sql)){
-    $date = date_create($rows['date_sched']);
-    $boDe = date_create($rows['book_date']);
-$customer=$rows['customer_id'];
-$service=$rows['service_id'];
-    $sqli=mysqli_query($con,"SELECT * FROM customer WHERE customer_id=$customer;");
-    $reCus = mysqli_fetch_assoc($sqli);
-    $sqli=mysqli_query($con,"SELECT * FROM services WHERE service_id=$service;");
-    $reSer = mysqli_fetch_assoc($sqli);
-    
-    //$reCus['f_name'].' '.$reCus['l_name'].' '.$reSer['service_name'].' '.date_format($date, 'M/d/Y g:i A')
-?>
-    { Name: "<?php echo $reCus['f_name'].'<br>'.$reCus['l_name'].'<br>'.$reSer['service_name'].'<br>'.date_format($date, 'M/d/Y g:i A');?>"},
-<?php } ?>
-]; // Can be obtained from another source, such as your objJson variable
-//{ adName: "AdName 1"}, | listing_table.innerHTML += objJson[i].adName + "<br>";
-function prevPage()
-{
-    if (current_page > 1) {
-        current_page--;
-        changePage(current_page);
-    }
-}
-
-function nextPage()
-{
-    if (current_page < numPages()) {
-        current_page++;
-        changePage(current_page);
-    }
-}
-    
-function changePage(page)
-{
-    var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");
-
-    var listing_table = document.getElementById("listingTable");
-    var page_span = document.getElementById("page");
- 
-    // Validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
-
-    listing_table.innerHTML = "";
-
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
-        listing_table.innerHTML += objJson[i].Name + "<br>";
-    }
-    page_span.innerHTML = page + "/" + numPages();
-
-    if (page == 1) {
-        btn_prev.style.visibility = "hidden";
-    } else {
-        btn_prev.style.visibility = "visible";
-    }
-
-    if (page == numPages()) {
-        btn_next.style.visibility = "hidden";
-    } else {
-        btn_next.style.visibility = "visible";
-    }
-}
-
-function numPages()
-{
-    return Math.ceil(objJson.length / records_per_page);
-}
-
-window.onload = function() {
-    changePage(1);
-};  
-</script>
+<!-- Bootstrap Core JavaScript -->
+<script src="../js/bootstrap.min.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="../js/startmin.js"></script>
+<!-- jQuery -->
+<script src="../js/jquery.min.js"></script>
 <script>
 n =  new Date();
 y = n.getFullYear();
@@ -165,7 +133,5 @@ m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
 </script>
-<script src="../js/pagination.js"></script>
-<script src="../js/pagination.min.js"></script>
 </body>
 </html>
