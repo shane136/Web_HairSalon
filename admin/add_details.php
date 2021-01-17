bb13579ac5845585465ce6a426fee81a0fd9735a
shane136 <preashane5@gmail.com>
2021-01-17 11:02:02 +0800

    tarong2

--- admin/add_details.php
+++ admin/add_details.php
@@ -25,9 +25,9 @@ $row = mysqli_fetch_assoc($sql);
 
 <div class="col-2 border border-danger h-100 flex-column d-flex"style="height:50px;background: #ffe6e6 !important;">
 
-<a href="\Web_HairSalon\admin\index.php" class=" btn btn-outline-light pt-0" style=""><p class="m-0" style="color:black; font-size:115%; text-align:center;"><strong>Home</strong></p></a>
+<a href="\Web_HairSalon\admin\index.php" class=" btn btn-outline-light pt-0" style=""><p class="m-0" style="color:black; font-size:115%; text-align:left;"> <i class="fas fa-home"></i><strong> Home</strong></p></a>
 
-<a href="\Web_HairSalon\conn\logout.php" onclick="return confirm('Are you sure you want to log out?');" class=" btn btn-outline-light pt-0" style=""><p class="m-0" style="color:black; font-size:100%; text-align:center;"><small>Logout</small></p></a>
+<a href="\Web_HairSalon\conn\logout.php" onclick="return confirm('Are you sure you want to log out?');" class=" btn btn-outline-light pt-0" style=""><p class="m-0" style="color:black; font-size:100%; text-align:left;"> <i class="fas fa-sign-out-alt"></i><small> Logout</small></p></a>
 </div>
 
 <div class="container mh-100 p-3" style="background: #0F222D;height:30vh;">
